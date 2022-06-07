2264e9391daf0320be9b8757901269aa7fde060f
unknown <AstrocricketGaming@gmail.com>
2022-06-07 14:31:46 -0500

    Removed Inventory From The Whole Project

--- Assets/Scripts/Interactables/Keypad.cs
+++ Assets/Scripts/Interactables/Keypad.cs
@@ -4,28 +4,10 @@ using UnityEngine;
 
 public class Keypad : Interactable
 {
-
-    public Inventory inventory;
     public GameObject ItemButton;
    
     protected override void Interact()
     {
         Debug.Log("Interacted With " + gameObject.name);
-        
-        for (int i = 0; i < inventory.slots.Length; i++)
-        {
-            if (inventory.IsFull[i] == false)
-            {
-                // Add The Object To The Inventory
-
-                inventory.IsFull[i] = true;
-
-                Instantiate(ItemButton, inventory.slots[i].transform, false);
-
-                Destroy(gameObject);
-
-                break;
-            }
-        }
     }
 }
